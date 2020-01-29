4f16521fe48b6a70e27e57811b5f23ea62f7daca
Bradley Meyer <mbradle@clemson.edu>
2020-01-29 08:16:14 -0500

    Simplify master.h download.

--- run_single_zone.sh
+++ run_single_zone.sh
@@ -3,6 +3,7 @@
 if [ "$HEADER_COPY_DIRECTORY" ]
 then
   cp /my-projects/single_zone/default/master.h ${HEADER_COPY_DIRECTORY}/master.h
+else
+  ./single_zone_network $VAR
 fi
 
-./single_zone_network $VAR
